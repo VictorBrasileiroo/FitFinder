32068453f2d559d69616fe0ba0203e1fe4108a73
Your Name <victor.lopesvfk11@gmail.com>
2024-10-23 10:35:53 -0300

    add gitpages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FitFinder/', // Substitua "nome-do-repositorio" pelo nome do seu repositório no GitHub
 })
